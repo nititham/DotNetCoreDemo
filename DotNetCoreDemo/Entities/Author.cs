e0b40ea89dafb47e75f31cabadb6dd5a9c2347e4
sukrit@iconext.co.th <sukrit@iconext.co.th>
2018-11-17 14:06:35 +0700

    install EF Core Power Tools => reverse engineering : create database class

---
+++ DotNetCoreDemo/Entities/Author.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNetCoreDemo.Entities
+{
+    public partial class Author
+    {
+        public Author()
+        {
+            Post = new HashSet<Post>();
+        }
+
+        public int AuthorId { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Post> Post { get; set; }
+    }
+}
